22881849f003febee96bd8e931344a74ee0b6025
Ihar <ihar.myshkevich134@alu.uhu.es>
2021-05-05 22:27:03 +0200

    Home page almost finished

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,9 +17,20 @@ export class HomePage {
   }
 
   ngOnInit(){
-    this.getData().subscribe(res=>
-        this.datos = res
-      )
+    this.getData().subscribe(res=>{
+      this.datos = res;
+      if(this.datos["Group background"]["type"] == "img"){
+        document.getElementById("group_info").style.backgroundImage = "url(../../assets/images/group_info_background.png)";
+        document.getElementById("group_info").style.backgroundRepeat = "no-repeat";
+        document.getElementById("group_info").style.backgroundSize = "cover";
+        document.getElementById("group_info").style.backgroundPosition = "center";
+        document.getElementById("group_info").style.color = this.datos["Group background"]["text color"];
+      }else{
+        document.getElementById("group_info").style.backgroundColor = this.datos["Group background"]["color"];
+        document.getElementById("group_info").style.color = this.datos["Group background"]["text color"];
+      }
+    }
+    )
   }
 
   getData(){
